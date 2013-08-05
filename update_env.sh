53fb3df9a8f330441800113ac4aef6fc8efa74d0
Roberto Gandolfo Hashioka <roberto_hashioka@hotmail.com>
2013-08-05 14:36:32 -0700

    Added config files and installation file

---
+++ update_env.sh
@@ -0,0 +1,8 @@
+#Install and configure the environment 
+sudo apt-get update ; sudo apt-get install -y zsh git tmux
+sudo chsh -s /bin/zsh vagrant
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+mv robertohashioka.zsh-theme .oh-my-zsh/themes/
+mv .* ~/
+cat id_rsa.pub >> .ssh/authorized_keys
+rm -rf id_rsa.pub
