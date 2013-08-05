a6656b8241884612ea51bf67f0b5240617838188
Roberto Gandolfo Hashioka <roberto_hashioka@hotmail.com>
2013-08-05 14:53:53 -0700

    Update installation file

--- update_env.sh
+++ update_env.sh
@@ -2,7 +2,9 @@
 sudo apt-get update ; sudo apt-get install -y zsh tmux
 sudo chsh -s /bin/zsh vagrant
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-mv robertohashioka.zsh-theme .oh-my-zsh/themes/
-mv .* ~/
-cat id_rsa.pub >> .ssh/authorized_keys
+mv robertohashioka.zsh-theme ~/.oh-my-zsh/themes/
+mv .tmux.conf ~/
+mv .zshrc ~/
+cat id_rsa.pub >> ~/.ssh/authorized_keys
 rm -rf id_rsa.pub
+tmux &
